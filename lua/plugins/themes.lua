1195d512888268555908200f3b9ce74a781a9f24
deadmade <130925709+deadmade@users.noreply.github.com>
2025-03-31 14:12:40 +0200

    Update themes.lua

--- lua/plugins/themes.lua
+++ lua/plugins/themes.lua
@@ -1,11 +1,9 @@
 return {
-  { "nvim-lualine/lualine.nvim" },
-  { "morhetz/gruvbox" },
-  { "rose-pine/neovim",              name = "rose-pine" },
-  { "rebelot/kanagawa.nvim",         name = "kanagawa" },
-  { "drewtempelmeyer/palenight.vim", name = "palenight" },
-  { "huyvohcmc/atlas.vim",           name = "atlas" },
-  { "catppuccin/nvim",               name = "catppuccin", priority = 1000 },
-  { "lunarvim/templeos.nvim",        name = "templeos" },
-  { "alexxGmZ/e-ink.nvim",           name = "e-ink",      priority = 1000 },
+  "zaldih/themery.nvim",
+  lazy = false,
+  config = function()
+    require("themery").setup({
+      themes = { "gruvbox"}, -- Your list of installed colorschemes.
+      livePreview = true }) -- Apply theme while picking. Default to true.    
+  end
 }
