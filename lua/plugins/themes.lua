ccc53f4940aee341a246d554c113cbd72f972c85
deadmade <130925709+deadmade@users.noreply.github.com>
2025-03-31 14:09:00 +0200

    changed themes

--- lua/plugins/themes.lua
+++ lua/plugins/themes.lua
@@ -1,9 +1,11 @@
 return {
-  "zaldih/themery.nvim",
-  lazy = false,
-  config = function()
-    require("themery").setup({
-      themes = { "tokyonight", "gruvbox", "ayu", "catppuccin", "kanagawa-lotus", "kanagawa-dragon"}, -- Your list of installed colorschemes.
-      livePreview = true }) -- Apply theme while picking. Default to true.    
-  end
+  { "nvim-lualine/lualine.nvim" },
+  { "morhetz/gruvbox" },
+  { "rose-pine/neovim",              name = "rose-pine" },
+  { "rebelot/kanagawa.nvim",         name = "kanagawa" },
+  { "drewtempelmeyer/palenight.vim", name = "palenight" },
+  { "huyvohcmc/atlas.vim",           name = "atlas" },
+  { "catppuccin/nvim",               name = "catppuccin", priority = 1000 },
+  { "lunarvim/templeos.nvim",        name = "templeos" },
+  { "alexxGmZ/e-ink.nvim",           name = "e-ink",      priority = 1000 },
 }
