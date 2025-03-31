aeda901b89c7a8a06b3016df2847212ab96fe903
deadmade <130925709+deadmade@users.noreply.github.com>
2025-03-31 19:54:55 +0200

    improved gitsigns

--- init.lua
+++ init.lua
@@ -56,19 +56,7 @@ require('nixCatsUtils.lazyCat').setup(nixCats.pawsible({"allPlugins", "start", "
 --require 'plugins.vim-sleuth',
 --require 'plugins.comment',
 
--- See `:help gitsigns` to understand what the configuration keys do
-{ -- Adds git related signs to the gutter, as well as utilities for managing changes
-  'lewis6991/gitsigns.nvim',
-  opts = {
-    signs = {
-      add = { text = '+' },
-      change = { text = '~' },
-      delete = { text = '_' },
-      topdelete = { text = '‾' },
-      changedelete = { text = '~' },
-    },
-  },
-},
+require 'plugins.gitsigns',
 
 --require 'plugins.which-key',
 require 'plugins.telescope',
