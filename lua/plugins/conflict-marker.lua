88346b57a1a399abfb792c76ecd5dc478c0c4a63
deadmade <130925709+deadmade@users.noreply.github.com>
2025-04-04 18:05:57 +0200

    performance

--- lua/plugins/conflict-marker.lua
+++ lua/plugins/conflict-marker.lua
@@ -1,1 +1,8 @@
-return {'rhysd/conflict-marker.vim'} -- Highlight conflict markers in git files
+return {
+    'rhysd/conflict-marker.vim',
+    event = 'BufRead', -- Load only when opening a file
+    cond = function()
+        return vim.fn.isdirectory(vim.fn.getcwd() .. '/.git') == 1
+      end,
+
+} -- Highlight conflict markers in git files
