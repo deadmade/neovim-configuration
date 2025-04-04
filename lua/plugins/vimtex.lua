50c6f67282e238c3f1e94987c8a8ced1f06db3ca
deadmade <130925709+deadmade@users.noreply.github.com>
2025-04-04 18:11:54 +0200

    performance

--- lua/plugins/vimtex.lua
+++ lua/plugins/vimtex.lua
@@ -1,5 +1,10 @@
 return {
     "lervag/vimtex",
+    event = 'BufRead',  -- Load only when opening a file
+    cond = function()
+      -- Check if the file is a LaTeX file or if the current directory is part of a LaTeX project
+      return vim.fn.expand('%:e') == 'tex'
+  end,
     lazy = false,     -- we don't want to lazy load VimTeX
     -- tag = "v2.15", -- uncomment to pin to a specific release
     init = function()
