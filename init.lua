28fd8db078f8f26efc833c6a118c07c2505b304a
deadmade <130925709+deadmade@users.noreply.github.com>
2025-04-05 17:16:55 +0200

    Update init.lua

--- init.lua
+++ init.lua
@@ -50,54 +50,55 @@ local lazyOptions = {
 -- NOTE: Here is where you install your plugins.
 -- NOTE: nixCats: this the lazy wrapper. Use it like require('lazy').setup() but with an extra
 -- argument, the path to lazy.nvim as downloaded by nix, or nil, before the normal arguments.
-require('nixCatsUtils.lazyCat').setup(nixCats.pawsible({"allPlugins", "start", "lazy.nvim" }),
-{
+require('nixCatsUtils.lazyCat').setup(nixCats.pawsible({ "allPlugins", "start", "lazy.nvim" }),
+  {
 
---require 'plugins.ltex',
-require 'plugins.vim-sleuth',
-require 'plugins.comment',
+    --require 'plugins.ltex',
+    require 'plugins.vim-sleuth',
+    require 'plugins.comment',
+    require 'plugins.obsidian',
 
-require 'plugins.gitsigns',
-require 'plugins.conflict-marker',
-require 'plugins.lualine',
+    require 'plugins.gitsigns',
+    require 'plugins.conflict-marker',
+    require 'plugins.lualine',
 
 
-require 'plugins.which-key',
-require 'plugins.telescope',
-require 'plugins.lsp',
-require 'plugins.autoformat',
-require 'plugins.autocompletion',
-require 'plugins.themes',
+    require 'plugins.which-key',
+    require 'plugins.telescope',
+    require 'plugins.lsp',
+    require 'plugins.autoformat',
+    require 'plugins.autocompletion',
+    require 'plugins.themes',
 
-require 'plugins.treesitter',
-require 'plugins.smalltools',
-require 'plugins.vimtex',
+    require 'plugins.treesitter',
+    require 'plugins.smalltools',
+    require 'plugins.vimtex',
 
--- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
--- init.lua. If you want these files, they are in the repository, so you can just download them and
--- place them in the correct locations.
+    -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
+    -- init.lua. If you want these files, they are in the repository, so you can just download them and
+    -- place them in the correct locations.
 
--- NOTE: Next step on your Neovim journey: Add/Configure additional plugins for Kickstart
---
---  Here are some example plugins that I've included in the Kickstart repository.
---  Uncomment any of the lines below to enable them (you will need to restart nvim).
---
--- NOTE: nixCats: instead of uncommenting them, you can enable them
--- from the categories set in your packageDefinitions in your flake or other template!
--- This is because within them, we used nixCats to check if it should be loaded!
---require 'kickstart.plugins.debug',
---require 'kickstart.plugins.indent_line',
---require 'kickstart.plugins.lint',
---require 'kickstart.plugins.autopairs',
---require 'kickstart.plugins.neo-tree',
---require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
+    -- NOTE: Next step on your Neovim journey: Add/Configure additional plugins for Kickstart
+    --
+    --  Here are some example plugins that I've included in the Kickstart repository.
+    --  Uncomment any of the lines below to enable them (you will need to restart nvim).
+    --
+    -- NOTE: nixCats: instead of uncommenting them, you can enable them
+    -- from the categories set in your packageDefinitions in your flake or other template!
+    -- This is because within them, we used nixCats to check if it should be loaded!
+    --require 'kickstart.plugins.debug',
+    --require 'kickstart.plugins.indent_line',
+    --require 'kickstart.plugins.lint',
+    --require 'kickstart.plugins.autopairs',
+    --require 'kickstart.plugins.neo-tree',
+    --require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
 
--- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
---    This is the easiest way to modularize your config.
---
---  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
---    For additional information, see `:help lazy.nvim-lazy.nvim-structuring-your-plugins`
-{ import = 'custom.plugins' },
-}, lazyOptions)
+    -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
+    --    This is the easiest way to modularize your config.
+    --
+    --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
+    --    For additional information, see `:help lazy.nvim-lazy.nvim-structuring-your-plugins`
+    { import = 'custom.plugins' },
+  }, lazyOptions)
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
