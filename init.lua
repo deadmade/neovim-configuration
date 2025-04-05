1d39ccff8263f709cf4ff95495fa908001116a85
deadmade <manuel.schuelein@proton.me>
2025-04-05 22:33:33 +0200

    fix

--- init.lua
+++ init.lua
@@ -71,7 +71,7 @@ require("nixCatsUtils.lazyCat").setup(nixCats.pawsible({ "allPlugins", "start", 
 	require("plugins.treesitter"),
 	require("plugins.smalltools"),
 	require("plugins.vimtex"),
-  require("plugins.hlchunk")
+  require("plugins.hlchunk"), 
 	-- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
 	-- init.lua. If you want these files, they are in the repository, so you can just download them and
 	-- place them in the correct locations.
