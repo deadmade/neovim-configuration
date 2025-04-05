f5f0d39a33e82b8595399c6a1f7d3b0151b0ded0
deadmade <manuel.schuelein@proton.me>
2025-04-05 21:59:53 +0200

    perf

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -1,56 +1,60 @@
 return {
-    "epwalsh/obsidian.nvim",
-    version = "*", -- recommended, use latest release instead of latest commit
-    lazy = true,
-    event = { 'BufReadPre', 'BufNewFile' },
-    ft = "markdown",
-    dependencies = {
-        "nvim-lua/plenary.nvim",
-    },
-    opts = {
-        workspaces = {
-            {
-                name = "notes",
-                -- Path to your Obsidian vault.
-                path = "~/Documents/notes",
-                -- Optional, if you want to use a different file extension for your notes.
-                extension = ".md",
-            },
-        },
-        completion = {
-            -- Enable completion with nvim-cmp.
-            nvim_cmp = true,
-            -- Trigger completion at 2 chars.
-            min_chars = 2,
-        },
+	"epwalsh/obsidian.nvim",
+	version = "*", -- recommended, use latest release instead of latest commit
+	lazy = true,
+	event = { "BufReadPre", "BufNewFile" },
+	cond = function()
+		-- Check if the file is a LaTeX file or if the current directory is part of a LaTeX project
+		return vim.fn.expand("%:e") == "tex"
+	end,
+	ft = "markdown",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+	},
+	opts = {
+		workspaces = {
+			{
+				name = "notes",
+				-- Path to your Obsidian vault.
+				path = "~/Documents/notes",
+				-- Optional, if you want to use a different file extension for your notes.
+				extension = ".md",
+			},
+		},
+		completion = {
+			-- Enable completion with nvim-cmp.
+			nvim_cmp = true,
+			-- Trigger completion at 2 chars.
+			min_chars = 2,
+		},
 
-        ui = {
-            enable = true
-        },
+		ui = {
+			enable = true,
+		},
 
-        picker = {
-            -- Set your preferred picker. Can be one of 'telescope.nvim', 'fzf-lua', or 'mini.pick'.
-            name = "telescope.nvim",
-            -- Optional, configure key mappings for the picker. These are the defaults.
-            -- Not all pickers support all mappings.
-            note_mappings = {
-                -- Create a new note from your query.
-                new = "<C-x>",
-                -- Insert a link to the selected note.
-                insert_link = "<C-l>",
-            },
-            tag_mappings = {
-                -- Add tag(s) to current note.
-                tag_note = "<C-x>",
-                -- Insert a tag at the current location.
-                insert_tag = "<C-l>",
-            },
-        },
+		picker = {
+			-- Set your preferred picker. Can be one of 'telescope.nvim', 'fzf-lua', or 'mini.pick'.
+			name = "telescope.nvim",
+			-- Optional, configure key mappings for the picker. These are the defaults.
+			-- Not all pickers support all mappings.
+			note_mappings = {
+				-- Create a new note from your query.
+				new = "<C-x>",
+				-- Insert a link to the selected note.
+				insert_link = "<C-l>",
+			},
+			tag_mappings = {
+				-- Add tag(s) to current note.
+				tag_note = "<C-x>",
+				-- Insert a tag at the current location.
+				insert_tag = "<C-l>",
+			},
+		},
 
-        disable_frontmatter = true, -- Disable metadata frontmatter
-        note_id_func = function(title)
-            -- Use the title as the filename instead of generating an ID
-            return title:gsub(" ", "_"):lower()
-        end,
-    },
+		disable_frontmatter = true, -- Disable metadata frontmatter
+		note_id_func = function(title)
+			-- Use the title as the filename instead of generating an ID
+			return title:gsub(" ", "_"):lower()
+		end,
+	},
 }
