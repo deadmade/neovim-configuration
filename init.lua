bd1967e9fab6c04cb050ddce496c29d7b7648fa2
deadmade <manuel.schuelein@proton.me>
2025-07-14 18:16:44 +0200

    added kickstart files

--- init.lua
+++ init.lua
@@ -65,12 +65,12 @@ require("nixCatsUtils.lazyCat").setup(nixCats.pawsible({ "allPlugins", "start", 
 	-- NOTE: nixCats: instead of uncommenting them, you can enable them
 	-- from the categories set in your packageDefinitions in your flake or other template!
 	-- This is because within them, we used nixCats to check if it should be loaded!
-	--require 'kickstart.plugins.debug',
-	--require 'kickstart.plugins.indent_line',
-	--require 'kickstart.plugins.lint',
-	--require 'kickstart.plugins.autopairs',
+	require 'kickstart.plugins.debug',
+	require 'kickstart.plugins.indent_line',
+	require 'kickstart.plugins.lint',
+	require 'kickstart.plugins.autopairs',
 	require("kickstart.plugins.neo-tree"),
-	--require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
+	require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
 
 	-- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
 	--    This is the easiest way to modularize your config.
--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,26 +2,26 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {
-	--require 'ltex',
-	require("vim-sleuth"),
-	require("comment"),
-	require("obsidian"),
+-- return {
+-- 	--require 'ltex',
+-- 	require("vim-sleuth"),
+-- 	require("comment"),
+-- 	require("obsidian"),
 
-	require("gitsigns"),
-	require("conflict-marker"),
-	require("lualine"),
+-- 	require("gitsigns"),
+-- 	require("conflict-marker"),
+-- 	require("lualine"),
 
-	require("which-key"),
-	require("telescope"),
-	require("lsp"),
-	require("autoformat"),
-	require("autocompletion"),
-	require("themes"),
+-- 	require("which-key"),
+-- 	require("telescope"),
+-- 	require("lsp"),
+-- 	require("autoformat"),
+-- 	require("autocompletion"),
+-- 	require("themes"),
 
-	require("treesitter"),
-	require("smalltools"),
-	require("vimtex"),
-    require("hlchunk"), 
-    require("copilot"),
-}
+-- 	require("treesitter"),
+-- 	require("smalltools"),
+-- 	require("vimtex"),
+--     require("hlchunk"), 
+--     require("copilot"),
+-- }
